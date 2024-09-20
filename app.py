70708c477c46c17e7682e0e5080d9e2531c69867
sotraptis <sraptis@uom.edu.gr>
2024-09-20 15:52:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 import pydicom
-import cv2
+from PIL import Image, ImageOps, ImageDraw
 import streamlit as st
 
 # Φόρτωση του εκπαιδευμένου μοντέλου πρόβλεψης καρκίνου
@@ -13,19 +13,19 @@ model = tf.keras.models.load_model(model_path, compile=False)
 def process_image(file):
     dicom = pydicom.dcmread(file)
     img = dicom.pixel_array
-    if len(img.shape) == 2:  # Έλεγχος αν είναι 2D εικόνα
-        img = np.expand_dims(img, axis=-1)  # Προσθήκη άξονα καναλιού
-    img = (img - np.min(img)) / (np.max(img) - np.min(img))  # Κανονικοποίηση
-    img = tf.image.resize(img, (256, 256))  # Αλλαγή μεγέθους
+    img = (img - np.min(img)) / (np.max(img) - np.min(img)) * 255  # Κανονικοποίηση
+    img = Image.fromarray(img).convert('L')  # Μετατροπή σε grayscale με Pillow
+    img = ImageOps.fit(img, (256, 256))  # Αλλαγή μεγέθους σε 256x256
     img = np.expand_dims(img, axis=0)  # Προσθήκη batch dimension
+    img = np.expand_dims(img, axis=-1)  # Προσθήκη καναλιού
     return img
 
 # Λειτουργία για επικάλυψη της περιοχής ενδιαφέροντος (ROI) πάνω στην εικόνα
 def overlay_roi_on_image(image, roi):
-    roi_colored = np.zeros_like(image)
-    roi_colored[roi > 0] = [255, 0, 0]  # Κόκκινο για την περιοχή με καρκίνο
-    overlay = cv2.addWeighted(image, 0.7, roi_colored, 0.3, 0)
-    return overlay
+    image = image.convert("RGB")
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([(roi[0], roi[1]), (roi[2], roi[3])], outline="red", width=3)
+    return image
 
 # Streamlit UI
 st.title("Πρόβλεψη Καρκίνου από DICOM Εικόνες")
@@ -45,15 +45,14 @@ if uploaded_file is not None:
     # Εάν η πρόβλεψη είναι 'Καρκίνος', δημιουργούμε και εμφανίζουμε την περιοχή ενδιαφέροντος (ROI)
     if prediction_label == 'Καρκίνος':
         dicom_image = pydicom.dcmread(uploaded_file).pixel_array
-        dicom_image = cv2.normalize(dicom_image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+        dicom_image = (dicom_image - np.min(dicom_image)) / (np.max(dicom_image) - np.min(dicom_image)) * 255
+        dicom_image = Image.fromarray(dicom_image).convert('L')
 
         # Δημιουργία μιας απλής ROI για την προβολή
-        roi = np.zeros_like(dicom_image)
-        h, w = dicom_image.shape
-        roi[h//4:h//2, w//4:w//2] = 1  # Χονδρική εκτίμηση περιοχής καρκίνου
+        roi = (64, 64, 192, 192)  # Χονδρική εκτίμηση περιοχής καρκίνου
 
         # Επικάλυψη της ROI στην αρχική εικόνα
-        overlay_image = overlay_roi_on_image(cv2.cvtColor(dicom_image, cv2.COLOR_GRAY2BGR), roi)
+        overlay_image = overlay_roi_on_image(dicom_image, roi)
 
         # Εμφάνιση της εικόνας
         st.image(overlay_image, caption="Περιοχή του καρκίνου", use_column_width=True)
