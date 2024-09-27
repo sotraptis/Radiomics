91c396fb0bdb5b4e0c684392475f58524170cb9e
sotraptis <sraptis@uom.edu.gr>
2024-09-27 12:34:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ shap_features = [
 # Συνάρτηση για εμφάνιση της αρχικής σελίδας
 def show_home_page():
     st.markdown("""
-        <div style='float: right; margin-right: 50px;'>
+        <div style='display: flex; justify-content: flex-end;'>
             <img src='static/raptis2.webp' width='160'>
         </div>
     """, unsafe_allow_html=True)
@@ -51,8 +51,8 @@ def show_home_page():
     st.markdown("<h1>R.A.P.T.I.S.</h1>", unsafe_allow_html=True)
     st.markdown("<h2>Radiomics Assisted Prognostication and Targeting Intelligence System</h2>", unsafe_allow_html=True)
     st.image('static/test.gif', use_column_width=False)
-    st.markdown("<div style='clear: both;'></div>", unsafe_allow_html=True)  # Ensure layout integrity
     uploaded_files = st.file_uploader("Upload DICOM files", type=["dcm"], accept_multiple_files=True)
+
 
     
     if uploaded_files and st.button("Upload and Predict"):
